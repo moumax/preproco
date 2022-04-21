4ecb1e0243b635554f2200e12a51ce6f689bf9db
Marc LANTOL <marc.lantol@gmail.com>
2022-04-21 12:14:01 +0200

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img className="movie-picture" src="src/movie.jpg" alt="movie"></img>
+        <div className="prez">
+          <div className="title">Movie title</div>
+          <h2 className="title-synopsis">Movie synopsis</h2>
+          <p className="synopsis">lorem*10</p>
+        </div>
+        <h2 className="title-subtitle">Subtitle</h2>
+        <buttons className="buttons">Item1</buttons>
+        <buttons className="buttons">Item2</buttons>
+        <buttons className="buttons">Item3</buttons>
+        <buttons className="buttons">Item4</buttons>
+      </div>
     </div>
   );
 }
