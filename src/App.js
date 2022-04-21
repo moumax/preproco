15c606fbfbd63ca56fd2d66a4dda87a4a5167e34
Marc LANTOL <marc.lantol@gmail.com>
2022-04-21 14:24:25 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import "./style.scss";
+import movie from "../src/movie.jpg"; // with import
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <img className="movie-picture" src="src/movie.jpg" alt="movie"></img>
+        <img className="movie-picture" src={movie} alt="movie"></img>
         <div className="prez">
-          <div className="title">Movie title</div>
+          <h1 className="title">Movie title</h1>
           <h2 className="title-synopsis">Movie synopsis</h2>
-          <p className="synopsis">lorem*10</p>
+          <p className="synopsis">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
         </div>
         <h2 className="title-subtitle">Subtitle</h2>
-        <buttons className="buttons">Item1</buttons>
-        <buttons className="buttons">Item2</buttons>
-        <buttons className="buttons">Item3</buttons>
-        <buttons className="buttons">Item4</buttons>
+        <div className="container-button">
+          <button className="buttons">Item1</button>
+          <button className="buttons">Item2</button>
+          <button className="buttons">Item3</button>
+          <button className="buttons">Item4</button>
+        </div>
       </div>
     </div>
   );
